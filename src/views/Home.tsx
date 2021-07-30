6d874c0435dbe3d542e323667e4233883b973417
Zayne <llz981011@hotmail.com>
2021-07-30 21:15:26 +0100

    extract Home page from `App.tsx`

---
+++ src/views/Home.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+export function Home() {
+	const history = useHistory();
+
+	const currentUser = sessionStorage.getItem("currentUser");
+	console.log(currentUser);
+	// redirect to login page if no current user in session storage
+	if (!currentUser) history.push("/login");
+
+	// useEffect(() => {});
+
+	return (
+		<div className="router-view ">
+			<p>
+				Edit <code>src/App.tsx</code> and save to reload.
+			</p>
+			<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer">
+				Learn React
+			</a>
+		</div>
+	);
+}
