c90dfe7534cb5460c98ae171104a598bd9d9e161
Zayne <llz981011@hotmail.com>
2021-08-01 22:25:27 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,7 @@ app.post("/api/auth/register", (req, res) => {});
 
 app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client/build/index.html"));
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
