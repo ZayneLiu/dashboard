f165f70658f0eb0ca02b608fe69835133e03984a
Zayne <llz981011@hotmail.com>
2021-08-01 18:04:28 +0100

    api server setup

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "path";
+// rest of the code remains same
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "client/build")));
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.join((__dirname = "client/build/index.html")));
+// 	});
+// }
+
+app.get("/api", (req, res) => {
+	res.send({ msg: "/" });
+});
+
+app.post("/api/dashboard");
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+	console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
