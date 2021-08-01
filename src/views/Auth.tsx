dcbfb8395f145c8e85026a70530694990c4f2465
Zayne <llz981011@hotmail.com>
2021-08-01 18:34:01 +0100

    cleanup for merge

--- src/views/Auth.tsx
+++
@@ -1,7 +0,0 @@
-import { Login } from "./Components/LoginComponent";
-import { SignUp } from "./Components/RegisterComponent";
-import "./Auth.scss";
-
-export function Auth(props: any) {
-	return props.isRegister ? SignUp() : Login();
-}
