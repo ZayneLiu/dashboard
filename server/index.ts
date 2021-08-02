8e11cba7161113faf9da0efa165a780ec0ecf1b2
Zayne <llz981011@hotmail.com>
2021-08-02 01:55:36 +0100

    [WIP] API server
    data flow done,
    
    - file upload
    - actual login & register

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,36 @@
 import express from "express";
+import { json } from "body-parser";
 import path from "path";
+import { UserSchema } from "./src/models/UserModel";
+
 // rest of the code remains same
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "client/build")));
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.join((__dirname = "client/build/index.html")));
-// 	});
-// }
+app.use(json({ type: "application/json" }));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("/api", (req, res) => {
 	res.send({ msg: "/" });
 });
 
-app.post("/api/auth/login", (req, res) => {});
-app.post("/api/auth/register", (req, res) => {});
+// TODO: File upload
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.post("/api/register", (req, res) => {
+	const data: UserSchema = req.body;
+	const { _id, ...registerInfo } = data;
+	// TODO:
+	console.log(registerInfo);
+});
+
+app.post("/api/login", (req, res) => {
+	const data: UserSchema = req.body;
+	const { _id, username, profileImg, ...loginInfo } = data;
+	// TODO:
+	console.log(loginInfo);
+});
+
+// `catch-all` route to serve react app
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
