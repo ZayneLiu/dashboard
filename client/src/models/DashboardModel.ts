cf6fbeb89625b2033643b5e11cc7fa4b422157c9
Zayne <llz981011@hotmail.com>
2021-08-03 21:47:09 +0100

    Update DashboardModel.ts

--- client/src/models/DashboardModel.ts
+++ client/src/models/DashboardModel.ts
@@ -1,4 +1,12 @@
 export class DashboardModel {
+	public async getWeather(location: GeolocationPosition) {
+		const { latitude, longitude } = location.coords;
+		const key = "541e426aa47c6281aad333b4d7b0ba76";
+		const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`;
+
+		const res = await fetch(url);
+		return res.json();
+	}
 	public async getNews() {
 		const res = await fetch("/api/news", {
 			method: "GET",
@@ -6,4 +14,6 @@ export class DashboardModel {
 		});
 		return await res.json();
 	}
+
+	public async getSportData() {}
 }
