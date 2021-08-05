37cc1439866b422602727d05ac0258c769376f82
Zayne <llz981011@hotmail.com>
2021-08-05 20:20:07 +0100

    Update index.ts
    file upload
    image hosting

--- server/index.ts
+++ server/index.ts
@@ -1,29 +1,38 @@
 import express from "express";
 import { json } from "body-parser";
 import path from "path";
+import fileupload, { UploadedFile } from "express-fileupload";
+
 import UserModel, { UserSchema } from "./src/models/UserModel";
 import { getSportData } from "./src/utils/SportData";
 import { getNewsByUrl, getNewsFeed } from "./src/utils/NewsFeed";
 
-// rest of the code remains same
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// middleware
 app.use(json({ type: "application/json" }));
+app.use(
+	fileupload({
+		useTempFiles: true,
+		tempFileDir: "/tmp/",
+		createParentPath: true,
+	})
+);
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("/api", (req, res) => {
-	res.send({ msg: "/" });
-});
-
+// models
 const model = new UserModel();
 
-// TODO: File upload
+// routes
+app.get("/image/:filename", (req, res) => {
+	const filename = req.params["filename"];
+	res.sendFile(__dirname + `/uploads/${filename}`);
+});
 
 app.post("/api/register", async (req, res) => {
 	const data: UserSchema = req.body;
 	const { _id, ...registerInfo } = data;
-
 	// TODO: check duplicates accounts
 
 	await model.setup();
@@ -57,11 +66,37 @@ app.get("/api/news/:newsId", async (req, res) => {
 	res.json(await getNewsByUrl(url));
 });
 
+app.post("/upload", async (req, res) => {
+	const uploadPath = __dirname + "/uploads/";
+
+	if (!req.files) {
+		res.json({ msg: "no file uploaded" });
+	}
+
+	if (req.files!.profileImg) {
+		const img = req.files!.profileImg as UploadedFile;
+
+		// set the filename to the files md5 value
+		const ext = path.extname(img.name).toLowerCase();
+		img.name = `${img.md5}${ext}`;
+
+		// move uploaded file to uploads directory
+		await img.mv(uploadPath + img.name);
+
+		// send back the filename to client
+		res.json({ profileImg: img.name });
+		return;
+	}
+
+	res.json({ msg: "no file uploaded" });
+});
+
 // `catch-all` route to serve react app
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+// start server
 app.listen(PORT, () => {
 	console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
