528f7cdcae7c89adb41b40743b5759cf560576fa
Zayne <llz981011@hotmail.com>
2021-08-06 17:34:06 +0100

    Photos page

--- client/src/views/Photos.tsx
+++ client/src/views/Photos.tsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { createRef, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import { message } from "antd";
 
-import "./Photos.scss";
 import AddPicture from "./../assets/Add_picture.png";
+import AddButton from "./../assets/Plus_button.png";
+import FileModel from "../models/FileModel";
+import UserModel, { UserSchema } from "../models/UserModel";
+import PhotoList from "./Components/PhotoListComponent";
+import "./Photos.scss";
 
 export function Photos(props: any) {
 	const history = useHistory();
+	const model = new FileModel();
+	const userModel = new UserModel();
+
+	const photoUploadRef = createRef<HTMLInputElement>();
+
+	const { _id } = JSON.parse(
+		sessionStorage.getItem("currentUser")!
+	) as UserSchema;
+
+	function uploadBtnOnclick() {
+		photoUploadRef.current?.click();
+	}
+
+	async function photoUploadOnchange() {
+		console.log(photoUploadRef.current!.files![0]);
+		const messageKey = "upload";
+		message.loading({ content: "image uploading", key: messageKey });
+
+		const img = photoUploadRef.current!.files![0];
+		const image = await model.uploadFile(img);
+		console.log(image);
+
+		message.success({ content: "uploaded", key: messageKey });
+
+		// get existing photos
+		let { photos } = await userModel.getUser({ _id });
+		// initialize photos if is doesn't exist
+		if (!photos) photos = [];
+		// append newly added photo
+		photos.push(image);
+		// update user info
+		await userModel.updateUser(_id!, { photos });
+
+		// setPhotos((await model.getPhotos(_id!)).photos);
+	}
+
 	return (
 		<div className="photos-page router-view">
 			<div className="title">Photos</div>
@@ -17,23 +58,19 @@ export function Photos(props: any) {
 			</button>
 
 			<div className="content">
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
-				<img src={AddPicture} alt="" />
+				<PhotoList _id={_id} />
+
+				<div className="add-btn" onClick={uploadBtnOnclick}>
+					<input
+						ref={photoUploadRef}
+						type="file"
+						accept="image/*"
+						name="new-picture"
+						onChange={photoUploadOnchange}
+					/>
+					<img src={AddPicture} alt="" />
+					<img className="add-btn-pic" src={AddButton} alt="" />
+				</div>
 			</div>
 		</div>
 	);
