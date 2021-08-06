8d85d8bf21606ee74ad133d99eec5b87383db0e7
Zayne <llz981011@hotmail.com>
2021-08-06 17:33:48 +0100

    Create PhotoListComponent.tsx

---
+++ client/src/views/Components/PhotoListComponent.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import FileModel from "../../models/FileModel";
+import { ObjectId, UserSchema } from "../../models/UserModel";
+import "./PhotoListComponent.scss";
+
+export default function PhotList(props: any) {
+	const {
+		_id,
+		limit,
+	}: { _id: ObjectId | undefined; limit: number | undefined } = props;
+	const model = new FileModel();
+
+	const [photos, setPhotos] = useState<string[]>();
+
+	useEffect(() => {
+		if (photos) return;
+
+		model.getPhotos(_id!).then((user) => {
+			if (!user.photos) setPhotos([]);
+			else setPhotos(user.photos!);
+		});
+	});
+
+	function getPhotos(): string[] {
+		if (limit) return photos?.slice(0, limit)!;
+		else return photos!;
+	}
+	// const { _id } = JSON.parse(
+	//     sessionStorage.getItem("currentUser")!
+	// ) as UserSchema;
+
+	return (
+		<>
+			{getPhotos()?.map((item, index) => {
+				return (
+					<div className="image-wrapper" key={index}>
+						<img src={`/image/${item}`} alt="" />
+					</div>
+				);
+			})}
+		</>
+	);
+}
