cffcde376853b50ed88fbcce4ed4aa29728be70d
Zayne <llz981011@hotmail.com>
2021-08-06 01:37:38 +0100

    Update DashboardModel.ts
    weather API: http to https

--- client/src/models/DashboardModel.ts
+++ client/src/models/DashboardModel.ts
@@ -2,19 +2,13 @@ export class DashboardModel {
 	public async getWeather(location: GeolocationPosition) {
 		const { latitude, longitude } = location.coords;
 		const key = "541e426aa47c6281aad333b4d7b0ba76";
-		const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`;
+		const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`;
 
-		const res = await fetch(url, {
-			method: "GET",
-			headers: {},
-		});
+		const res = await fetch(url);
 		return res.json();
 	}
 	public async getNews() {
-		const res = await fetch("/api/news", {
-			method: "GET",
-			headers: {},
-		});
+		const res = await fetch("/api/news");
 		return res.json();
 	}
 
