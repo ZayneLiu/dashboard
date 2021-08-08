47810c0919d0830ee9c218f4f0bf49b370fade05
Zayne <llz981011@hotmail.com>
2021-08-08 18:04:01 +0100

    Create UserModel.test.ts

---
+++ client/src/models/UserModel.test.ts
@@ -0,0 +1,8 @@
+// import { ObjectId } from "bson";
+import UserModel, { UserSchema } from "../models/UserModel";
+
+// const model = new UserModel();
+
+test("get tasks", async () => {
+	// await model.getTasks(new ObjectId("610c323fcc566b16b6b7f72b"));
+});
